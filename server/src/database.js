839bd1ddcf5fa4230dcba093d66729b0591feb82
kwhong95 <kwhong95@kakao.com>
2021-08-02 20:46:16 +0900

    [DEV]Create database - convert csv to json in nodejs

---
+++ server/src/database.js
@@ -0,0 +1,27 @@
+const csvToJson = require("convert-csv-to-json")
+
+const database = {
+	cafe: []
+}
+
+Object.keys(database).forEach((key) => {
+	database[key] = [
+		...database[key],
+		...csvToJson.fieldDelimiter(',')
+		.getJsonFromCsv(`./src/data-in-csv/${key}.csv`)
+	]
+	if (database[key].length > 0) {
+		const firstItem = database[key][0];
+		Object.keys(firstItem).forEach((itemKey) => {
+			if (database[key].every((item) => {
+				return /^-?\d+$/.test(item[itemKey])
+			})) {
+				database[key].forEach((item) => {
+					item[itemKey] = Number(item[itemKey])
+				})
+			}
+		})
+	}
+})
+
+module.exports = database;
