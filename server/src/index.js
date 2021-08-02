a7a1d742d6e2b20bbc0dd15a5c503efbb56ab2e8
kwhong95 <kwhong95@kakao.com>
2021-08-02 21:02:30 +0900

    [DEV]Create Apollo Server - graphQL

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,27 @@
 const database = require('./database')
+const { ApolloServer, gql } = require('apollo-server')
 
-console.log(database);
+const typeDefs = gql`
+	type Query {
+		cafe: [Cafe]
+	}
+	type Cafe {
+		id: Int
+		name: String
+		region: String
+		address: String
+		contact: String
+	}
+`;
+
+const resolvers = {
+	Query: {
+		cafe: () => database.cafe
+	}
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+	console.log(`Server ready at ${url}`)
+})
