4e29a90c5fc32747e7b95bceced856911bbf4827
kwhong95 <kwhong95@kakao.com>
2021-08-09 03:43:58 +0900

    [DEV]Divide HomePage Layout with CSS Grid

--- cafeteria-app/src/pages/HomePage.tsx
+++ cafeteria-app/src/pages/HomePage.tsx
@@ -1,9 +1,44 @@
 import React from "react";
 import IPageProps from "../interfaces/page";
+import styled from "@emotion/styled";
 
 const HomePage: React.FunctionComponent<IPageProps> = props => {
 
-  return <p>Homepage</p>
+
+  return (
+    <Container>
+      <Header>Header</Header>
+      <Interface>
+        interface
+      </Interface>
+      <MapContainer>
+        map
+      </MapContainer>
+    </Container>
+  )
 }
 
 export default HomePage;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 10vh 90vh;
+  grid-template-columns: 30vw 70vw;
+  border: 1px solid red;
+  height: 100vh;
+`
+
+const Header = styled.div`
+  grid-column: 1 / 5;
+  border: 1px solid blue;
+`
+
+const Interface = styled.div`
+  border: 1px solid orange;
+  
+`
+
+const MapContainer = styled.div`
+  border: 1px solid seagreen;
+  grid-column: 2 / 5;
+`
